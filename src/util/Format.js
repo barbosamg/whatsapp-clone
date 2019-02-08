b1ce27c91a968b56d9d2980f875226d7b06f2e39
Mateus Barbosa <mateus.barbosa@shift.com.br>
2019-02-08 17:47:43 -0200

    microfone

--- src/util/Format.js
+++ src/util/Format.js
@@ -6,4 +6,17 @@ class Format {
         div.innerHTML = `<div data-${text}="id"></div>`;
         return Object.keys(div.firstChild.dataset)[0];
     }
+
+    static toTime(duration) {
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        } else {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
+    }
+
 }
